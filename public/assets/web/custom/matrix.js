9801015fddcde0434b845a113e1a79917a3781cd
rmdobariya <roshan.crestcoder@gmail.com>
2024-02-01 16:08:28 +0530

    all calendar done

--- public/assets/web/custom/matrix.js
+++ public/assets/web/custom/matrix.js
@@ -106,4 +106,21 @@ $(document).on('click', '.add_to_apple_calendar', function () {
         console.log('Request failed: ' + textStatus)
     })
 });
+$(document).on('click', '.add_to_micro_soft_calendar', function () {
+    var title = $(this).data('rec-title');
+    var desc = $(this).data('rec-des');
+    loaderView()
+    $.ajax({
+        url: APP_URL + '/add-to-microsoft-calendar/' + title + '/' + desc,
+        method: 'GET',
+
+    }).done(function (data) {
+        loaderHide()
+        window.open(data.url, '_blank');
+
+    }).fail(function (jqXHR, textStatus) {
+        loaderHide()
+        console.log('Request failed: ' + textStatus)
+    })
+});
 
